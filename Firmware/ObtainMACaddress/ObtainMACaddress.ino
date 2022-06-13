d93fffc25b73d91722ca61e3e10448f12fa65a6c
nickyandre <105607046+nickyandre@users.noreply.github.com>
2022-06-13 21:08:56 +0200

    Add files via upload

---
+++ Firmware/ObtainMACaddress/ObtainMACaddress.ino
@@ -0,0 +1,12 @@
+// This code will be used to obtain MAC Address of the Taltech Piezopump
+#include "WiFi.h"
+ 
+void setup(){
+  Serial.begin(115200);
+  WiFi.mode(WIFI_MODE_STA);
+  Serial.println(WiFi.macAddress());
+}
+ 
+void loop(){
+
+}
